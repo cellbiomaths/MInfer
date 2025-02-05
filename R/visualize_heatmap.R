afcace7b1f0977c8bd1f37c2b887b1f4e1c0bb40
JanaSchwarzerova <61159143+JanaSchwarzerova@users.noreply.github.com>
2025-02-05 05:48:35 +0100

    Update visualize_heatmap.R
    add axis

--- R/visualize_heatmap.R
+++ R/visualize_heatmap.R
@@ -1,21 +1,29 @@
-#' Visualize Jacobian Matrix as Heatmap
-#'
-#' Plots a heatmap of the given matrix with a custom color palette.
-#' @param matrix The matrix to visualize.
-#' @param title The title of the heatmap.
-#' @export
-visualize_heatmap <- function(matrix, title = "Heatmap") {
-    color_palette <- colorRampPalette(c("blue", "white", "red"))(100)
-    max_val <- max(abs(matrix), na.rm = TRUE)
-    min_val <- -max_val
+library(MASS)
+library(pheatmap)
+library(rgl)
 
-    pheatmap(
-        matrix,
-        cluster_rows = FALSE,
-        cluster_cols = FALSE,
-        display_numbers = FALSE,
-        color = color_palette,
-        breaks = seq(min_val, max_val, length.out = 101),
-        main = title
+#' Visualize Jacobian Difference in 3D
+#'
+#' Creates a 3D perspective plot for the given matrix.
+#' @param matrix The matrix to visualize.
+#' @param metabolites_fin Vector of metabolite names for axis labels.
+#' @export
+visualize_3d <- function(matrix, metabolites_fin) {
+    color_palette <- colorRampPalette(c("blue", "white", "red"))(100)
+    z_values <- matrix
+    zlim <- range(z_values, na.rm = TRUE)
+    color_index <- cut(z_values, breaks = 100, labels = FALSE)
+    color_index[is.na(color_index)] <- 1
+
+    persp3d(
+        x = 1:ncol(matrix),
+        y = 1:nrow(matrix),
+        z = z_values,
+        col = color_palette[color_index],
+        xlab = "Metabolites",
+        ylab = "Metabolites",
+        zlab = "J(f)",
+        aspect = c(1, 1, 0.5),
+        theta = 30, phi = 30
     )
 }
